a9f54bada83d5d9ec538cb05d649e37cd5173329
Hozaifa Abdalla <abdallahozaifa19527@gmail.com>
2017-07-11 19:05:37 +0000

    Adding and removing of queue on tv screen

--- branch.js
+++ branch.js
@@ -2,6 +2,9 @@
 $(document).ready(function() {
     var yPosTop = 25;
     var container = $(".ATM-Pic")[0];
+    var options = {};
+    var reqArr = [];
+    var personArr = [];
 
     /* Creates a component on the page */
     var createComp = function(txt) {
@@ -12,12 +15,62 @@ $(document).ready(function() {
         h2.append(span);
         $(span).html(txt);
         $(container).append(h2);
+        //personArr.push(txt);
         yPosTop += 10;
     };
-    createComp("1. Hozaifa Abdalla");
-    createComp("2. Brendon James");
-    createComp("3. Sean Kirkland");
-    createComp("4. Sean Kirkland");
-    createComp("5. Sean Kirkland");
-    createComp("6. Sean Kirkland");
+
+    var genList = function() {
+        $(".comp").remove();
+        yPosTop = 25;
+
+        var nameCnt = 1;
+        personArr.forEach(function(name) {
+            createComp(nameCnt + ". " + name);
+            nameCnt++;
+        });
+    };
+
+    var removePerson = function(name) {
+        var indx, cnt=0;
+        personArr.forEach(function(nm) {
+            if(name == nm){
+                indx = cnt;
+            }
+            cnt++;
+        });
+        personArr.splice(indx, 1);
+        genList();
+    };
+    
+    var addPerson = function(name){
+        personArr.push(name);
+        genList();
+    }
+    addPerson("Hozaifa Abdalla");
+    addPerson("Sean Kirkland");
+    addPerson("Brendon James");
+    genList();
+    removePerson("Hozaifa Abdalla");
+    addPerson("Sean Kirkland");
+    addPerson("Sean dfgd");
+
+    options.onOpen = function(e) {
+        console.log("Connection Open");
+    };
+
+    options.onEnd = function(e) {
+        console.log("Connection Closed");
+        reqArr[0] = $.SSE('http://beaconapp-abdallahozaifa.c9users.io:8080/queue', options);
+        reqArr[0].start();
+    };
+
+    options.events = {
+        beacon: function(e) {
+            console.log("Received Data from Center");
+        }
+    };
+
+    var sse = $.SSE('http://beaconapp-abdallahozaifa.c9users.io:8080/event', options);
+    reqArr.push(sse);
+    //sse.start();
 });
